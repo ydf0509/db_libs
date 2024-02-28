e75a54319e0902d7c53abb58add64ecc5e25d674
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-02-28 14:29:14 +0800

    commit

---
+++ db_libs/scripts/sqlmodel.py
@@ -0,0 +1,8 @@
+
+
+from sqlmodel import create_engine, Session
+
+"""
+SQLModel 是一个基于 Python 的 SQL 数据库访问工具，
+它提供了一种声明式的方式来定义数据库模型，并且能够自动创建数据库表格、执行查询操作和数据持久化。SQLModel 基于 SQLAlchemy 库构建而成，它简化了 SQLAlchemy 的使用方式，使得开发者能够更加方便地与 SQL 数据库交互。
+"""
